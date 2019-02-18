db89f0ea79c4dbaff4b15b7549cff123b1ee954b
Robert Gooch <44298079+RobertGoochUK@users.noreply.github.com>
2019-02-18 14:58:06 +0000

    Update dbconfig.php

--- fhir/stu3/dbconfig.php
+++ fhir/stu3/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 $host = "{your host name}"; /* Host name */
 $user = "{your user name}"; /* User */
-$password = "{your password"; /* Password */
+$password = "{your password}"; /* Password */
 $dbname = "{your db name}"; /* Database name */
 ?>
