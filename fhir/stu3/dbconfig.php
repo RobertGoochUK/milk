b4e0ca13cc02d7569fa947041a5ad3b2a888f2b2
Robert Gooch <44298079+RobertGoochUK@users.noreply.github.com>
2019-02-18 14:57:33 +0000

    Update dbconfig.php

--- fhir/stu3/dbconfig.php
+++ fhir/stu3/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
-$host = "mymilkinstance.cd4dfr1gywzq.eu-west-2.rds.amazonaws.com"; /* Host name */
-$user = "masteruser"; /* User */
-$password = "MX7BKFKixc2VvrE"; /* Password */
-$dbname = "milk"; /* Database name */
+$host = "{your host name}"; /* Host name */
+$user = "{your user name}"; /* User */
+$password = "{your password"; /* Password */
+$dbname = "{your db name}"; /* Database name */
 ?>
