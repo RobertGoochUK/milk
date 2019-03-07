b38b4a11e432ebe225ddd0cdd5f3e860826fcaf5
Robert Gooch <44298079+RobertGoochUK@users.noreply.github.com>
2019-03-07 19:04:29 +0000

    Delete dbconfig.php

--- fhir/stu3/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "{your host name}"; /* Host name */
-$user = "{your user name}"; /* User */
-$password = "{your password}"; /* Password */
-$dbname = "{your db name}"; /* Database name */
-?>
