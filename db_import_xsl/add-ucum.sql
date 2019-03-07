9004d885a4d7e2fb2ebdccc84b3753da6c26040a
Robert Gooch <44298079+RobertGoochUK@users.noreply.github.com>
2019-03-07 19:12:13 +0000

    Delete add-ucum.sql

--- db_import_xsl/add-ucum.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO lookup VALUES
-('UCUM', 258683005, 'kilogram'),
-('UCUM', 258682000, 'g'),
-('UCUM', 258684004, 'milligram'),
-('UCUM', 258686002, 'ng'),
-('UCUM', 258685003, 'ug'),
-('UCUM', 258773002, 'milliliter'),
-('UCUM', 258770004, 'liter'),
-('UCUM', 258770004, 'l');
